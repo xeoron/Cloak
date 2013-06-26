8b85a1d55b69fee1a463ede91aee68d8e2ef158e
xeoron <aitsinformation@gmail.com>
2013-06-25 22:02:43 -0400

    v0.2.2
    added file checking
    removed the for-do loop

--- cloak.sh
+++ cloak.sh
@@ -4,42 +4,46 @@
 # Author: Jason Campisi
 # Date: 6/23/2013
 # License: GPL 2 or higher
+declare -r NAME="Cloak"
+declare -r VERSION="0.2.2"
 
-declare -r VERSION="0.2.1"
+option=""
+FILE=$2
 
-for arg in "$@"
-do
-   case $arg in
+ if [ ! -e $FILE  ]; then
+    echo " $NAME Error - this location does not exist: $2"
+    exit 1;
+ else 
+	option=$1
+ fi
+
+ case $option in
     -h|--help)
-        echo "Usage: $0 <option> /location/to/folder"
+        echo "Usage: $NAME <option> /location/to/folder"
         echo " "
-        echo "Cloak any file/folder from Finder and Spotlight on your Mac"
+        echo "$NAME any file/folder from Finder and Spotlight on your Mac"
         echo "-c --cloak location from Finder and Spotlight"
         echo "-u --uncloak location from Finder and Spotlight"
         echo "-v --version"
         echo "-h --help"
         echo " "
-        exit 0
         ;;
-
     -u|--uncloak)
         echo " Uncloaking $2 ..."
         chflags -H -R nohidden "$2"
         echo " Adding $2 location to Spotlight's index..."
-        mdimport $2
-        exit 0
+        mdimport $FILE
         ;; 
     -c|--cloak)
         echo " Cloaking $2 ..."
     	chflags -H -R hidden "$2"
     	echo " Removing $2 location from Spotlight's index..."
-    	mdutil $2 >/dev/null
-    	exit 0
+    	mdutil $FILE >/dev/null
         ;;
     -v|--version)
-        echo " Cloak version: $VERSION"
+        echo " $NAME version: $VERSION"
         echo " System requirements: Mac OSX.5 Leopard or higher"
     	;;
-   esac
-done
+ esac
+
 exit 0
