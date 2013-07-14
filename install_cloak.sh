1bae54dce677b192ea3342c9bca8c8d9c67cc1ba
xeoron <aitsinformation@gmail.com>
2013-07-14 13:30:40 -0400

    v0.2.3

--- install_cloak.sh
+++ install_cloak.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 #Author: Jason Campisi
 #Date: 6/30/2013
-#version 0.2.2
+#version 0.2.3
 #Released under the GPL v2 or higher
 NAME="cloak"
 EXT="sh"
@@ -10,10 +10,10 @@ echo "$FILE installer:";
 
  echo " Checking if '$FILE' exists in the current folder..."
    if [ ! -n "$FILE" ]; then
-      echo " $NAME.$EXT Error - File name is not set!"
+      echo " Error - Filename is not set!"
       exit 1;
    elif [ ! -e "$FILE" ]; then
-      echo " Error - The location of '$NAME.$EXT' does not exist!"
+      echo " Error - The location of '$FILE' does not exist!"
       exit 1;
    fi
  echo " ...found!";
