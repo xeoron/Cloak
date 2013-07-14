79715bc29fed7e5d598db45aab2049f5dec06ee1
xeoron <aitsinformation@gmail.com>
2013-07-14 13:27:50 -0400

    v0.2.3
    made code easier to make changes

--- install_cloak.sh
+++ install_cloak.sh
@@ -1,28 +1,29 @@
 #!/bin/sh -e
 #Author: Jason Campisi
 #Date: 6/30/2013
-#version 0.2.3
+#version 0.2.2
 #Released under the GPL v2 or higher
 NAME="cloak"
 EXT="sh"
-echo "$NAME installer:";
+FILE="$NAME.$EXT"
+echo "$FILE installer:";
 
- echo " Checking if '$NAME.$EXT' exists in the current folder..."
-   if [ ! -n "$NAME.$EXT" ]; then
+ echo " Checking if '$FILE' exists in the current folder..."
+   if [ ! -n "$FILE" ]; then
       echo " $NAME.$EXT Error - File name is not set!"
       exit 1;
-   elif [ ! -e "$NAME.$EXT" ]; then
+   elif [ ! -e "$FILE" ]; then
       echo " Error - The location of '$NAME.$EXT' does not exist!"
       exit 1;
    fi
  echo " ...found!";
  
 echo " Setting file to executable...";
-chmod +x ./$NAME.$EXT
+chmod +x ./$FILE
 
 echo " Checking if you have the clearance to install this ...";
 if [ "$(whoami)" != 'root' ]; then
-	echo " You do not have permission to install './$NAME.$EXT'";
+	echo " You do not have permission to install './$FILE'";
 	echo " ->You must be a root user.";
 	echo " ->Try instead: sudo $0";
 	exit 1;
@@ -31,7 +32,7 @@ else
 fi
 
 echo " Installing $NAME to /usr/bin/ ...";
-cp ./$NAME.$EXT /usr/bin/$NAME
+cp ./$FILE /usr/bin/$NAME
 echo " Setup complete."
 echo "Testing install with this command\n>$NAME --version";
 $NAME --version
